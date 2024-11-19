2b5ad4561b12bf4e590174d7bdc8f98f4aeef47d
luis-keny <lucerobalvinluiskeny@gmail.com>
2024-11-19 17:54:07 -0500

    chore: change of cors policy

--- src/main/java/com/api/fatlab_backend/config/CorsConfig.java
+++ src/main/java/com/api/fatlab_backend/config/CorsConfig.java
@@ -14,8 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        // .allowedOrigins("http://localhost:4200", "https://fatlab-app-web.vercel.app")
-                        .allowedOrigins("*")
+                        .allowedOrigins("http://localhost:4200", "https://fatlab-app-web.vercel.app", "*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         // .allowCredentials(true);
